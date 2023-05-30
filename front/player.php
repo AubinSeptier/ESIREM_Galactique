32fb802216090f7e579972c8bab2ab5262460a7d
test_fond_1 <maxencecontant6@gmail.com>
2023-05-30 12:47:30 +0200

    created empire page

--- front/player.php
+++ front/player.php
@@ -18,33 +18,38 @@ if (!isset($_SESSION['username'])) {
     </header>
     <nav>
         <ul>
-            <li><a href="index.php">Accueil</a></li>
+            <li><a href="./index.php">Accueil</a></li>
             <li class="active"><a href="choix-univers.html">Choix de l'univers</a></li>
-            <li><a href="profil.html">Profil</a></li>
-            <li><a href="classement.html">Classement</a></li>
-            <li><a href="parametres.html">Paramètres</a></li>
+            <li><a href="./profil.html">Profil</a></li>
+            <li><a href="./classement.html">Classement</a></li>
+            <li><a href="./parametres.html">Paramètres</a></li>
         </ul>
     </nav>
     <section>
         <h2>Choix de l'univers</h2>
         <div class="univers">
-            <h3>Univers 1</h3>
-            <p>Description de l'univers 1.</p>
-            <a href="#" class="button">Choisir</a>
+            <h3>Créer un nouvel univers</h3>
+            <form>
+                <input type="text" name="nom" placeholder="Nom de l'univers" required>
+                <a href="./" class="button">Créer</a>
+            </form>
         </div>
         <div class="univers">
-            <h3>Univers 2</h3>
-            <p>Description de l'univers 2.</p>
-            <a href="#" class="button">Choisir</a>
-        </div>
-        <div class="univers">
-            <h3>Univers 3</h3>
-            <p>Description de l'univers 3.</p>
-            <a href="#" class="button">Choisir</a>
+            <h3>Se connecter à un univers existant</h3>
+            <form>
+                <select name="univers" required>
+                    <option value="" disabled selected>Choisir un univers</option>
+                    <option value="univers1">Univers 1</option>
+                    <option value="univers2">Univers 2</option>
+                    <option value="univers3">Univers 3</option>
+                </select>
+                <a href="#" class="button">Se connecter</a>
+            </form>
         </div>
     </section>
     <footer>
         <p>&copy; ESI Galactique 2023 - Tous droits réservés</p>
     </footer>
+    <script src="./js/create_universe.js"></script>
 </body>
 </html>
