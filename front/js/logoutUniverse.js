4d2323d38df29c513fc91a585532ef26eb26f201
test_fond_1 <maxencecontant6@gmail.com>
2023-06-01 14:41:11 +0200

    delete logout and replace by logoutUser/Universe.js

--- front/js/logoutUniverse.js
+++ front/js/logoutUniverse.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Récupérer la référence du bouton de déconnexion
   const logoutUniverseButton = document.querySelector('.logoutUniverse-button');
-  const logoutUserButton = document.querySelector('.logoutUser-button');
-
-
 
   // Ajouter un gestionnaire d'événement au clic sur le bouton de déconnexion
-  logoutUniverseButton.addEventListener('click', async function() {
+  logoutUniverseButton.addEventListener('click', async function(event) {
 
     // Effectuer une requête GET vers logout-process.php
     let response = await fetch('http://localhost/ESIREM_Galactique/api/process/disconnectUniverse-process.php', {
@@ -15,20 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(data.status);
     if(data.status === "success"){
         window.location.href = "http://localhost/ESIREM_Galactique/front/player.php";
-    }
-    else{
-        alert(data.status);
-    }
-  });
-
-  logoutUserButton.addEventListener('click', async function() {
-    // Effectuer une requête GET vers logout-process.php
-    let response = await fetch('http://localhost/ESIREM_Galactique/api/process/disconnectUser-process.php', {
-    });
-    let data = await response.json();
-    console.log(data.status);
-    if(data.status === "success"){
-        window.location.href = "http://localhost/ESIREM_Galactique/front/login.php";
     }
     else{
         alert(data.status);
