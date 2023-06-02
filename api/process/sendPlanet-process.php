ab5988cfa529b61b22887a47d63a992e9c5c4a9e
Aubin <aubinseptier@gmail.com>
2023-06-02 14:02:36 +0200

    Begin revise sendPlanet-process.php

--- api/process/sendPlanet-process.php
+++ api/process/sendPlanet-process.php
@@ -2,49 +2,17 @@
 include_once("../classes/solar_system.php");
 include_once("../classes/planet.php");
 
-$solarSystem = new SolarSystem();
+$solarSystem = new Solar_System();
 $planet = new Planet();
 
 if(isset($_GET["solarSystemName"])){
     $solarSystemName = $_GET["solarSystemName"];
-    $solarSystemId = $solarSystem->getSolarSystem($solarSystemName)[0]["id"];
+    $solarSystemId = $solarSystem->getSolar_System($solarSystemName)[0]["id"];
     $planetsCount = $planet->getPlanetsCount($solarSystemId);
     
     $planetData = $planet->getAllPlanets($solarSystemId);
 
     for($i = 0; $i < $planetsCount; $i++){
-        switch($i){
-            case 0:
-                $planet1 = $planetData[$i]["name"];
-                break;
-            case 1:
-                $planet2 = $planetData[$i]["name"];
-                break;
-            case 2:
-                $planet3 = $planetData[$i]["name"];
-                break;
-            case 3:
-                $planet4 = $planetData[$i]["name"];
-                break;
-            case 4:
-                $planet5 = $planetData[$i]["name"];
-                break;
-            case 5:
-                $planet6 = $planetData[$i]["name"];
-                break;
-            case 6:
-                $planet7 = $planetData[$i]["name"];
-                break;
-            case 7:
-                $planet8 = $planetData[$i]["name"];
-                break;
-            case 8:
-                $planet9 = $planetData[$i]["name"];
-                break;
-            case 9:
-                $planet10 = $planetData[$i]["name"];
-                break;
-        }
     }
     
 
