6fea00cd5c2e5372fa842df49a4ea908ff965750
Aubin <aubinseptier@gmail.com>
2023-06-03 12:40:32 +0200

    update fleet system in createShip-process.php

--- api/classes/fleet.php
+++ api/classes/fleet.php
@@ -11,7 +11,11 @@ include_once("database.php");
  */
 class Fleet extends Database {
         
-    // Récupérer une flotte par son id
+    /**
+    * @brief Obtenir la flotte par son id.
+    * @param $id L'id de la flotte.
+    * @return $result Une tableau des données de la flotte ou false si aucune n'a été trouvée.
+    */
     public function getFleet($id){
         $sql = "SELECT * FROM fleets WHERE id = ?";
         $query = $this->connect()->prepare($sql);
@@ -19,18 +23,45 @@ class Fleet extends Database {
         $result = $query->fetchAll();
         return $result;
     }
+
+    /**
+    * @brief Obtenir la flotte selon l'id de la planète.
+    * @param $id_planet L'id de la planète.
+    * @return $result Une tableau des données de la flotte de la planète ou false si aucune n'a été trouvée.
+    */
+    public function getFleetByPlanet($id_planet){
+        $sql = "SELECT * FROM fleets WHERE id_planet = ?";
+        $query = $this->connect()->prepare($sql);
+        $query->execute([$id_planet]);
+        $result = $query->fetchAll();
+        return $result;
+    }
     
-    // Ajouter une nouvelle flotte et ses caractéristiques
+    /**
+    * @brief Ajouter une flotte à la base de données.
+    * @param $name Le nom de la flotte.
+    * @param $ships_number Le nombre de vaisseaux dans la flotte.
+    * @param $attack L'attaque totale de la flotte.
+    * @param $defense La défense totale de la flotte.
+    * @param $id_empire L'id de l'empire auquel appartient la flotte.
+    * @param $id_planet L'id de la planète où se trouve la flotte.
+    */
     public function setFleet($name, $ships_number, $attack, $defense, $id_empire, $id_planet){
         $sql = "INSERT into fleets(name, ships_number, attack, defense, id_empire, id_planet) VALUES (?, ?, ?, ?, ?, ?)";
         $query = $this->connect()->prepare($sql);
         $query->execute([$name, $ships_number, $attack, $defense, $id_empire, $id_planet]);
     }
-
-    // Modifier les caractéristiques d'une flotte
-    public function updateFleet($name, $ships_number, $attack, $defense, $id_empire, $id_planet){
-        $sql = "UPDATE fleets SET name = ?, ships_number = ?, attack = ?, defense = ?, id_empire = ?, id_planet = ? WHERE id = ?";
+ 
+    /**
+    * @brief Mettre à jour de la flotte dans la base de données.
+    * @param $ships_number Le nombre de vaisseaux dans la flotte.
+    * @param $attack L'attaque totale de la flotte.
+    * @param $defense La défense totale de la flotte.
+    * @param $id_planet L'id de la planète où se trouve la flotte.
+    */
+    public function updateFleet($id_planet, $ships_number, $attack, $defense){
+        $sql = "UPDATE fleets SET ships_number = ?, attack = ?, defense = ? WHERE id_planet = ?";
         $query = $this->connect()->prepare($sql);
-        $query->execute([$name, $ships_number, $attack, $defense, $id_empire, $id_planet]);
+        $query->execute([$id_planet, $ships_number, $attack, $defense]);
     }
 }
--- api/process/createShip-process.php
+++ api/process/createShip-process.php
@@ -8,64 +8,122 @@ include_once("../classes/research.php");
 include_once("../classes/research_type.php");
 
 $research = new Research();
-$researchType = new ResearchType();
+$researchType = new Research_Type();
+$fleet = new Fleet();
+$empire = new Empire();
+$ship_type = new Ship_Type();
 
-if(isset($_GET["fighter"]) && isset($_GET["id_fleet"])){
-    $fighter = new Ship();
-    $id_fleet = $_GET["id_fleet"];
-    $shipCount = $fighter->getShipCountByType("fighter");
-    $shipNumber = $shipCount + 1;
-    $metal = $fighter->getShipType("fighter")[0]["metal_number"];
-    $deuterium = $fighter->getShipType("fighter")[0]["deuterium_number"];
-    $building_time = $fighter->getShipType("fighter")[0]["building_time"];
-    $attack = $fighter->getShipType("fighter")[0]["attack"];
-    $defense = $fighter->getShipType("fighter")[0]["defense"];
-    $capacity = $fighter->getShipType("fighter")[0]["capacity"];
+$deuteriumStock = $empire->getDeuteriumStock($_SESSION["empireId"]);  
+$metalStock = $empire->getMetalStock($_SESSION["empireId"]);
+$fighterId = $ship_type->getShip_Type("fighter")[0]["id"];
+$cruiserId = $ship_type->getShip_Type("cruiser")[0]["id"];
+$transporterId = $ship_type->getShip_Type("transporter")[0]["id"];
+$settlerId = $ship_type->getShip_Type("settler")[0]["id"];
 
-    $fighter->setShip("Chasseur #".$shipNumber, $attack, $defense, $capacity, $id_fleet, "fighter");
-}
-else if(isset($_GET["cruiser"]) && isset($_GET["id_fleet"])){
-    $cruiser = new Ship();
-    $id_fleet = $_GET["id_fleet"];
-    $shipCount = $cruiser->getShipCountByType("cruiser");
-    $shipNumber = $shipCount + 1;
-    $metal = $cruiser->getShipType("cruiser")[0]["metal_number"];
-    $deuterium = $cruiser->getShipType("cruiser")[0]["deuterium_number"];
-    $building_time = $cruiser->getShipType("cruiser")[0]["building_time"];
-    $attack = $cruiser->getShipType("cruiser")[0]["attack"];
-    $defense = $cruiser->getShipType("cruiser")[0]["defense"];
-    $capacity = $cruiser->getShipType("cruiser")[0]["capacity"];
-    
-    $cruiser->setShip("Croiseur #".$shipNumber, $attack, $defense, $capacity, $id_fleet, "cruiser");
-}
-else if(isset($_GET["transporter"]) && isset($_GET["id_fleet"])){
-    $transporter = new Ship();
-    $id_fleet = $_GET["id_fleet"];
-    $shipCount = $transporter->getShipCountByType("transporter");
-    $shipNumber = $shipCount + 1;
-    $metal = $transporter->getShipType("transporter")[0]["metal_number"];
-    $deuterium = $transporter->getShipType("transporter")[0]["deuterium_number"];
-    $building_time = $transporter->getShipType("transporter")[0]["building_time"];
-    $attack = $transporter->getShipType("transporter")[0]["attack"];
-    $defense = $transporter->getShipType("transporter")[0]["defense"];
-    $capacity = $transporter->getShipType("transporter")[0]["capacity"];
+if(isset($_GET["ship"]) && isset($_GET["id_planet"])){
+    $id_planet = $_GET["id_planet"];
+    $id_fleet = $fleet->getFleetByPlanet($id_planet)[0]["id"];
+    $fleetSize = $fleet->getFleetByPlanet($id_planet)[0]["ships_number"];
+    $fleetAttack = $fleet->getFleetByPlanet($id_planet)[0]["attack"];
+    $fleetDefense = $fleet->getFleetByPlanet($id_planet)[0]["defense"];
 
-    $transporter->setShip("Transporteur #".$shipNumber, $attack, $defense, $capacity, $id_fleet, "transporter");
-}
-else if(isset($_GET["settler"]) && isset($_GET["id_fleet"])){
-    $settler = new Ship();
-    $id_fleet = $_GET["id_fleet"];
-    $shipCount = $settler->getShipCountByType("settler");
-    $shipNumber = $shipCount + 1;
-    $metal = $settler->getShipType("settler")[0]["metal_number"];
-    $deuterium = $settler->getShipType("settler")[0]["deuterium_number"];
-    $building_time = $settler->getShipType("settler")[0]["building_time"];
-    $attack = $settler->getShipType("settler")[0]["attack"];
-    $defense = $settler->getShipType("settler")[0]["defense"];
-    $capacity = $settler->getShipType("settler")[0]["capacity"];
+    if($_GET["ship"] == "fighter"){
+        $fighter = new Ship();
+        
+        $metalCost = $ship_type->getShip_Type("fighter")[0]["metal_number"];
+        $deuteriumCost = $ship_type->getShip_Type("fighter")[0]["deuterium_number"];
+        $building_time = $ship_type->getShip_Type("fighter")[0]["building_time"];
 
-    $settler->setShip("Colon #".$shipNumber, $attack, $defense, $capacity, $id_fleet, "settler");
+        if($metalCost > $metalStock || $deuteriumCost > $deuteriumStock){
+            echo json_encode(array("status" => "Vous n'avez pas assez de ressources pour construire ce vaisseau"));
+            exit;
+        }
+        
+        $empire->updateMetalStock($metalStock - $metalCost, $_SESSION["empireId"]);
+        $empire->updateDeuteriumStock($deuteriumStock - $deuteriumCost, $_SESSION["empireId"]);
+        $attack = $ship_type->getShip_Type("fighter")[0]["attack"];
+        $defense = $ship_type->getShip_Type("fighter")[0]["defense"];
+        $capacity = $ship_type->getShip_Type("fighter")[0]["capacity"];
+        $shipCount = $fighter->getShipCountByType($fighterId);
+        $shipNumber = $shipCount + 1;
+        $fleetAttack += $attack;
+        $fleetDefense += $defense;
+        $fleetSize += 1;
+
+        $fighter->setShip("Chasseur #".$shipNumber, $attack, $defense, $capacity, $id_fleet, $fighterId);
+        $fleet->updateFleet($id_planet, $fleetSize, $fleetAttack, $fleetDefense);
+        echo json_encode(array("status" => "success", "fleet" => $id_fleet, "Taille" => $fleetSize + 1, "Attack" => $fleetAttack + $attack, "Defense" => $fleetDefense + $defense));
+    }
+    if($_GET["ship"] == "cruiser"){
+        $cruiser = new Ship();
+        
+        $metalCost = $ship_type->getShip_Type("cruiser")[0]["metal_number"];
+        $deuteriumCost = $ship_type->getShip_Type("cruiser")[0]["deuterium_number"];
+        $building_time = $ship_type->getShip_Type("cruiser")[0]["building_time"];
+
+        if($metalCost > $metalStock || $deuteriumCost > $deuteriumStock){
+            echo json_encode(array("status" => "Vous n'avez pas assez de ressources pour construire ce vaisseau"));
+            exit;
+        }
+        
+        $empire->updateMetalStock($metalStock - $metalCost, $_SESSION["empireId"]);
+        $empire->updateDeuteriumStock($deuteriumStock - $deuteriumCost, $_SESSION["empireId"]);
+        $attack = $ship_type->getShip_Type("cruiser")[0]["attack"];
+        $defense = $ship_type->getShip_Type("cruiser")[0]["defense"];
+        $capacity = $ship_type->getShip_Type("cruiser")[0]["capacity"];
+        $shipCount = $cruiser->getShipCountByType($cruiserId);
+        $shipNumber = $shipCount + 1;
+        
+        $cruiser->setShip("Croiseur #".$shipNumber, $attack, $defense, $capacity, $id_fleet, $cruiserId);
+        echo json_encode(array("status" => "success"));
+    }
+    if($_GET["ship"] == "transporter"){
+        $transporter = new Ship();
+        
+        $metalCost = $ship_type->getShip_Type("transporter")[0]["metal_number"];
+        $deuteriumCost = $ship_type->getShip_Type("transporter")[0]["deuterium_number"];
+        $building_time = $ship_type->getShip_Type("transporter")[0]["building_time"];
+        
+        if($metalCost > $metalStock || $deuteriumCost > $deuteriumStock){
+            echo json_encode(array("status" => "Vous n'avez pas assez de ressources pour construire ce vaisseau"));
+            exit;
+        }
+        
+        $empire->updateMetalStock($metalStock - $metalCost, $_SESSION["empireId"]);
+        $empire->updateDeuteriumStock($deuteriumStock - $deuteriumCost, $_SESSION["empireId"]);
+        $attack = $ship_type->getShip_Type("transporter")[0]["attack"];
+        $defense = $ship_type->getShip_Type("transporter")[0]["defense"];
+        $capacity = $ship_type->getShip_Type("transporter")[0]["capacity"];
+        $shipCount = $transporter->getShipCountByType($transporterId);
+        $shipNumber = $shipCount + 1;
+
+        $transporter->setShip("Transporteur #".$shipNumber, $attack, $defense, $capacity, $id_fleet, $transporterId);
+        echo json_encode(array("status" => "success"));
+    }
+    if($_GET["ship"] == "settler"){
+        $settler = new Ship();
+        
+        $metalCost = $ship_type->getShip_Type("settler")[0]["metal_number"];
+        $deuteriumCost = $ship_type->getShip_Type("settler")[0]["deuterium_number"];
+        $building_time = $ship_type->getShip_Type("settler")[0]["building_time"];
+        
+        if($metalCost > $metalStock || $deuteriumCost > $deuteriumStock){
+            echo json_encode(array("status" => "Vous n'avez pas assez de ressources pour construire ce vaisseau"));
+            exit;
+        }
+        
+        $empire->updateMetalStock($metalStock - $metalCost, $_SESSION["empireId"]);
+        $empire->updateDeuteriumStock($deuteriumStock - $deuteriumCost, $_SESSION["empireId"]);
+        $attack = $ship_type->getShip_Type("settler")[0]["attack"];
+        $defense = $ship_type->getShip_Type("settler")[0]["defense"];
+        $capacity = $ship_type->getShip_Type("settler")[0]["capacity"];
+        $shipCount = $settler->getShipCountByType($settlerId);
+        $shipNumber = $shipCount + 1;
+
+        $settler->setShip("Colon #".$shipNumber, $attack, $defense, $capacity, $id_fleet, $settlerId);
+        echo json_encode(array("status" => "success"));
+    }
 }
-else{
-    echo "Erreur lors de la création du vaisseau";
+else {
+    echo json_encode(array("status" => "Erreur lors de la construction du vaisseau"));
 }
