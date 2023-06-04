09558f553760a32c65514a29504b7b7f315c3b54
Aubin Septier <aubinseptier@gmail.com>
2023-06-04 12:46:10 +0200

    Generate doc

--- doc/html/search/all_c.js
+++ doc/html/search/all_c.js
@@ -4,14 +4,14 @@ var searchData=
   ['updatedeuteriumstock_1',['updateDeuteriumStock',['../class_empire.html#ad66a33afda9ef2b718dbbadcc6f41f9c',1,'Empire']]],
   ['updateenergystock_2',['updateEnergyStock',['../class_empire.html#a34112a437430a6803c97de034ba1576a',1,'Empire']]],
   ['updateenergystockused_3',['updateEnergyStockUsed',['../class_empire.html#a33763533c139425cea17d7567ff76c9e',1,'Empire']]],
-  ['updatefleet_4',['updateFleet',['../class_fleet.html#a451e0a1541fe656c3a4f6de5e5a72585',1,'Fleet']]],
-  ['updateinfrastructure_5',['updateInfrastructure',['../class_infrastructure.html#aa578e9bd021ae8b73a8256a3c133c993',1,'Infrastructure']]],
+  ['updatefleet_4',['updateFleet',['../class_fleet.html#a4b8c4cb5fe8fee8ac5f56601f774ccda',1,'Fleet']]],
+  ['updateinfrastructure_5',['updateInfrastructure',['../class_infrastructure.html#a3d8eeb0431aaa38ca20900de8c62aff8',1,'Infrastructure']]],
   ['updatemetalstock_6',['updateMetalStock',['../class_empire.html#a87ac4a4509379b11102d514816c69c32',1,'Empire']]],
   ['updateplanetname_7',['updatePlanetName',['../class_planet.html#a38b16951971f73b8a6874e5e1b846fec',1,'Planet']]],
   ['updateplanetowner_8',['updatePlanetOwner',['../class_planet.html#aa92a36afa06ce620609bf736794df9a9',1,'Planet']]],
   ['updateresearch_9',['updateResearch',['../class_research.html#a79e6032833b7c49c175d3f4a2ae97aba',1,'Research']]],
-  ['updateresource_10',['updateResource',['../class_resource.html#a32b1e0370bfba9a065d4b6fa20e81392',1,'Resource']]],
-  ['updateship_11',['updateShip',['../class_ship.html#a5bf75a3477447fef76fe3c608de6f03b',1,'Ship']]],
+  ['updateresource_10',['updateResource',['../class_resource.html#a2f56608bb820f29260af19e8a5740ec0',1,'Resource']]],
+  ['updateship_11',['updateShip',['../class_ship.html#ad40806b3bb6f89f85b396205d36212ae',1,'Ship']]],
   ['upgradeinfrastructure_2dprocess_2ephp_12',['upgradeInfrastructure-process.php',['../upgrade_infrastructure.html',1,'']]],
   ['user_13',['User',['../class_user.html',1,'']]]
 ];
--- doc/html/search/functions_3.js
+++ doc/html/search/functions_3.js
@@ -1,16 +1,16 @@
 var searchData=
 [
   ['setempire_0',['setEmpire',['../class_empire.html#aca41814925263cda30cf213a007c4c11',1,'Empire']]],
-  ['setfleet_1',['setFleet',['../class_fleet.html#a4020ead8b3a7ab0c0b0795d9a67b90f0',1,'Fleet']]],
+  ['setfleet_1',['setFleet',['../class_fleet.html#ac5cc0d2b54ff994d97fef3aa0304ce5d',1,'Fleet']]],
   ['setgalaxy_2',['setGalaxy',['../class_galaxy.html#aec1f8d91f71f85fa2378ce2343ce428c',1,'Galaxy']]],
-  ['setinfrastructure_3',['setInfrastructure',['../class_infrastructure.html#a5026a0f16311c9bcd6dc54428f47b721',1,'Infrastructure']]],
-  ['setinfrastructure_5ftype_4',['setInfrastructure_Type',['../class_infrastructure___type.html#aaf1cea42f3bf9a66180c63d3d72dfd08',1,'Infrastructure_Type']]],
+  ['setinfrastructure_3',['setInfrastructure',['../class_infrastructure.html#a21ab0480a56eb30c7ca03e68b582088f',1,'Infrastructure']]],
+  ['setinfrastructure_5ftype_4',['setInfrastructure_Type',['../class_infrastructure___type.html#aec119cdc943452a5f07fdff8662d9baa',1,'Infrastructure_Type']]],
   ['setplanet_5',['setPlanet',['../class_planet.html#a30a8ad824c30346c9ed0667315ec0634',1,'Planet']]],
   ['setresearch_6',['setResearch',['../class_research.html#a0a79e4bc499bb5208d4419edd3064d3c',1,'Research']]],
   ['setresearch_5ftype_7',['setResearch_Type',['../class_research___type.html#a604df2f8ea02bbb0813f22b7ffa09709',1,'Research_Type']]],
-  ['setresource_8',['setResource',['../class_resource.html#a61bfb0f250f7b99ebb4c268c3c6fa803',1,'Resource']]],
-  ['setship_9',['setShip',['../class_ship.html#ada87a0768cdcd33df70f4909c1b0bc81',1,'Ship']]],
-  ['setship_5ftype_10',['setShip_Type',['../class_ship___type.html#a53c8411ef9bc7d1d835736d15133cb0a',1,'Ship_Type']]],
+  ['setresource_8',['setResource',['../class_resource.html#a8befff5ecae18eaf239805c9f61c792a',1,'Resource']]],
+  ['setship_9',['setShip',['../class_ship.html#a07a02e093e0febf310b86fa1ffd4d0fa',1,'Ship']]],
+  ['setship_5ftype_10',['setShip_Type',['../class_ship___type.html#a9d3dfa3eb63d1a68e460a560dad661cf',1,'Ship_Type']]],
   ['setsolar_5fsystem_11',['setSolar_System',['../class_solar___system.html#afb9c215eb1af030dd595f3df139c5da0',1,'Solar_System']]],
   ['setuniverse_12',['setUniverse',['../class_universe.html#ac2aa22afb64c26a026cf381587f40f35',1,'Universe']]],
   ['setuser_13',['setUser',['../class_user.html#a25cac3f8d6712ac7371e5aec55d8eba9',1,'User']]]
