542ee869e1f7a89993b62263cf90b916cd2276d1
test_fond_1 <maxencecontant6@gmail.com>
2023-06-04 13:42:58 +0200

    add research page + some updates

---
+++ front/js/recherche.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const upgradeEnergy = document.querySelector('.research-energy');
+    const upgradeLaser = document.querySelector('.research-laser');
+    const upgradeIons = document.querySelector('.research-ions');
+    const upgradeShield = document.querySelector('.research-shield');
+    const updateArmament = document.querySelector('.research-armament');
+
+});
+
