195204a9bfea4170f8ad1789d189a716ce5b0931
test_fond_1 <maxencecontant6@gmail.com>
2023-06-04 15:34:07 +0200

    add research.js

--- front/js/recherche.js
+++ front/js/recherche.js
@@ -5,5 +5,77 @@ document.addEventListener('DOMContentLoaded', function() {
     const upgradeShield = document.querySelector('.research-shield');
     const updateArmament = document.querySelector('.research-armament');
 
+    let url = "http://localhost/ESIREM_Galactique/api/process/makeResearch-process.php";
+
+    upgradeEnergy.addEventListener('click', async function(event) {
+        let response = await fetch(url + "?research=energy", {
+            });
+        let data = await response.json();
+        console.log(data.status);
+        if(data.status==="success"){
+            alert("tech ENERGIE améliorée")
+        }    
+        else {
+            alert(data.status);
+        }
+
+    });
+
+    upgradeLaser.addEventListener('click', async function(event) {
+        let response = await fetch(url + "?research=laser", {
+            });
+        let data = await response.json();
+        console.log(data.status);
+        if(data.status==="success"){
+            alert("tech LASER améliorée")
+        }    
+        else {
+            alert(data.status);
+        }
+
+    });
+
+    upgradeIons.addEventListener('click', async function(event) {
+        let response = await fetch(url + "?research=ions", {
+            });
+        let data = await response.json();
+        console.log(data.status);
+        if(data.status==="success"){
+            alert("tech IONS améliorée")
+        }    
+        else {
+            alert(data.status);
+        }
+
+    });
+
+    upgradeShield.addEventListener('click', async function(event) {
+        let response = await fetch(url + "?research=shield", {
+            });
+        let data = await response.json();
+        console.log(data.status);
+        if(data.status==="success"){
+            alert("tech BOUCLIER améliorée")
+        }    
+        else {
+            alert(data.status);
+        }
+
+    });
+    
+    updateArmament.addEventListener('click', async function(event) {
+        let response = await fetch(url + "?research=armament",{
+            });
+        let data = await response.json();
+        console.log(data.status);
+        if(data.status==="success"){
+            alert("tech ARMEMENT améliorée")
+        }    
+        else {
+            alert(data.status);
+        }
+
+    });
+
 });
 
